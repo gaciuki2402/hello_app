04c2b36552f13da9b9cd588bfbbcbff61d7d21b4
gaciuki <delbrivigrace@gmail.com>
2024-01-11 21:17:52 +0300

    initial commit

--- test_project/lib/screens/dashboard.dart
+++ test_project/lib/screens/dashboard.dart
@@ -7,18 +7,12 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'.toUpperCase()),
-        backgroundColor: Colors.blueGrey,
-      ),
+    return const Scaffold(
+      backgroundColor: Colors.lightGreen,
       body: Center(
-        child: Text("Random Value is: ${getNumber()}")//Text.rich accepts the child of type TextSpan
-      ), //center has a property child which refers to the actual content
-    );//scaffold has two properties: body(specifies main user interface) and appBar(specifies header user interface)
+        child: Icon(Icons.email)
+        ),
+    ); //scaffold has two properties: body(specifies main user interface) and appBar(specifies header user interface)
   }
 }
 
-int getNumber(){
-  return Random().nextInt(100);
-}
